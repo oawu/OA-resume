a55843a4cf1c48fb34570b80c33704f52c23c7a8
OA Wu <comdan66@gmail.com>
2014-11-15 00:39:32 +0800

    Fix css font.

--- system/js/content.js
+++ system/js/content.js
@@ -21,13 +21,10 @@ $(function () {
   });
 
   $(window).scroll (function () {
+
     clearTimeout (scroll_timer);
     
     scroll_timer = setTimeout (function () {
-      // console.info ('$(window).scrollTop () : ' + $(window).scrollTop ());
-      // console.info ('.first top : ' + ($('.first').offset ().top - actionRange));
-      // console.info ('.first bottom : ' + ($('.first').offset ().top + $('.first').height () - actionRange));
-// children
       $('.panel').each (function () {
         if (!$(this).data ('active') && ($(window).scrollTop () > $(this).offset ().top - actionRange))
           $(this).data ('active', true).children ('.unit').each (function () {
@@ -44,15 +41,6 @@ $(function () {
             $(this).css ({'top': (parseFloat ($(this).data ('top'))) + 'px'}).removeClass ('active');
           });
       });
-      
-
-      // if (!$('.first').hasClass ('cancel') && ($(window).scrollTop () > 700))
-      //   $('.first').addClass ('cancel');
-
-    //   if ($('.user_avatar, .infos, .background').hasClass ('cancel') && ($(window).scrollTop () < 750))
-    //     $('.user_avatar, .infos, .background').removeClass ('cancel');
-
-
     }, 10);
 
     // console.info ($(window).scrollTop ());
