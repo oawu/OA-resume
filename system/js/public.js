0bad9755842bf6eefcfd322b048a50bcf1869c83
OA Wu <comdan66@gmail.com>
2014-11-16 21:12:31 +0800

    Fix Facebook code.

--- system/js/public.js
+++ system/js/public.js
@@ -22,6 +22,6 @@ window.fbAsyncInit = function() {
   var js, fjs = d.getElementsByTagName(s)[0];
   if (d.getElementById(id)) {return;}
   js = d.createElement(s); js.id = id;
-  js.src = "//connect.facebook.net/zh_TW/sdk.js";
+  js.src = "//connect.facebook.net/zh_TW/all.js";
   fjs.parentNode.insertBefore(js, fjs);
 }(document, 'script', 'facebook-jssdk'));
