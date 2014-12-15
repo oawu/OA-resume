bcba7518f8ea2101a9a6c06f8875043477186165
OA Wu <comdan66@gmail.com>
2014-12-15 18:39:51 +0800

    Temp.

--- js/index.js
+++ js/index.js
@@ -4,9 +4,70 @@
  */
 
 $(function () {
-  // ga ('send', 'event', 'test', 'send', document.URL);
-  // $('button').click (function () {
-  //   // ga ('send', 'event', 'test', 'send', document.URL);
-  // });
+  $container = $('#container').empty ();
+  $('#navbar').OAnavbar ();
+
+  $('.item').click (function () {
+    $(this).addClass ('active').siblings ().removeClass ('active');
+    window.location.assign ('#r' + $(this).index ());
+  });
+
+  var $active = $('.item').filter ('.active');
+  $active.length ? $active.click () : $('.item').eq (!location.hash ? 0 : location.hash.slice (location.hash.indexOf ('r') + 1)).click ();
+
+  $('.item').each (function (i) {
+    Path.map ("#r" + i).to (function () {
+      if ($('#_r' + i).length) $container.append ($('<div />').addClass ('r').append (_.template ($('#_r' + i).html (), {}) ({})));
+    }).enter (function () {
+      $container.children ('.r').addClass ('r-hide').bind ('animationend webkitAnimationEnd MSAnimationEnd oAnimationEnd', function () { $(this).remove (); });
+    });
+  });
+
+  Path.listen();
+
+  var scroll_timer = null;
+
+  // var panelHeight = 600;
+  // var topActionRange = 100;
+  // var containerTop = 50;
+
+  // var scroll_timer = null;
+  // var actionRange = panelHeight / 2;
+
+  // actionRange += containerTop;
+
+  $(window).scroll (function () {
+
+    clearTimeout (scroll_timer);
+// console.clear()
+
+    scroll_timer = setTimeout (function () {
+      $container.children ('.r').children ('.page').removeClass ('now').each (function () {
+        if ($(this).offset ().top - $(window).scrollTop () < $(window).height () / 2) {
+          $(this).addClass ('now').prev ('.page').removeClass ('now');
+console.info ($(this).index ());
+
+        }
+      });
+
+    //   $('.panel').each (function () {
+    //     if (!$(this).data ('active') && ($(window).scrollTop () > $(this).offset ().top - actionRange))
+    //       $(this).data ('active', true).children ('.unit').each (function () {
+    //         $(this).css ({'top': (parseFloat ($(this).data ('top')) - topActionRange) + 'px'}).addClass ('active');
+    //       });
+
+    //     if (($(this).data ('active') == true) && ($(window).scrollTop () > ($(this).offset ().top + $(this).height () - actionRange)))
+    //       $(this).data ('active', false).children ('.unit').each (function () {
+    //         $(this).css ({'top': (parseFloat ($(this).data ('top')) - topActionRange * 2) + 'px'}).removeClass ('active');
+    //       });
+
+    //     if (($(this).data ('active') == true) && ($(window).scrollTop () < $(this).offset ().top - actionRange))
+    //       $(this).data ('active', false).children ('.unit').each (function () {
+    //         $(this).css ({'top': (parseFloat ($(this).data ('top'))) + 'px'}).removeClass ('active');
+    //       });
+    //   });
+    }, 100);
+
+  }.bind (this));
 
 });
