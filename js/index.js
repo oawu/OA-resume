cdf58265bd5bfa4d1345a20806352baa11b9b8db
OA Wu <comdan66@gmail.com>
2014-12-16 08:00:49 +0800

    temp

--- js/index.js
+++ js/index.js
@@ -3,7 +3,56 @@
  * @copyright   Copyright (c) 2014 OA Wu Design
  */
 
+var tag = document.createElement('script');
+    tag.src = "https://www.youtube.com/player_api";
+    var firstScriptTag = document.getElementsByTagName('script')[0];
+    firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+var yt_p1_3 = null;
+var yt_p1_4 = null;
+var is_load = null;
+
+function onYouTubePlayerAPIReady () {
+  yt_p1_3 = new YT.Player(
+      'yt_p1_3',
+      {
+          videoId: ['nIYbbFzesec'],
+          events: {
+        
+              'onReady'        : function () {},
+              'onStateChange'  : function () {},
+              'onError'        : function () {},
+          }
+      }
+  );
+  yt_p1_4 = new YT.Player(
+      'yt_p1_4',
+      {
+          videoId: ['nIYbbFzesec'],
+          events: {
+        
+              'onReady'        : function () {},
+              'onStateChange'  : function () {},
+              'onError'        : function () {},
+          }
+      }
+  );
+  is_load = true;
+
+}
+var stop = function ($obj) {
+  if ($obj.length && $obj.data ('is_enable'))
+    eval ($obj.attr ('id') + '.pauseVideo()');  
+}
+var play = function ($obj) {
+  if ($obj.length && $obj.data ('is_enable', true)) {
+    eval ($obj.attr ('id') + '.playVideo()');  
+    eval ($obj.attr ('id') + '.mute()');  
+  }
+}
+
 $(function () {
+
   $container = $('#container').empty ();
   $('#navbar').OAnavbar ();
 
@@ -37,14 +86,20 @@ $(function () {
       scroll_timer = setTimeout (function () {
         clearTimeout (scroll_timer);
         $(window).scroll (function () {
-          console.error (123);
-          $container.children ('.r').children ('.page').removeClass ('now').each (function () {
+          $container.children ('.r').children ('.page').removeClass ('now').removeClass ('ed').each (function () {
             if (($(this).offset ().top - $(window).scrollTop ()) < ($(window).height () / 2) && !$(this).hasClass ('now')) {
               now_index = $(this).index ();
             }
           });
+          $container.children ('.r').children ('.page').map (function () {
+            stop ($(this).find ('[id^="yt_"]'));
+          });
+          var $ed = $container.children ('.r').children ('.page:lt(' + (now_index) + ')').addClass ('ed')
+
           var $now = $container.children ('.r').children ('.page').eq (now_index).addClass ('now');
-          if (index != now_index) {
+          play ($now.find ('[id^="yt_"]'));
+          
+          if (index != now_index && $now.length) {
             index = now_index;
             scrollTo ($now.offset ().top -  range, 100)
           }
@@ -55,7 +110,7 @@ $(function () {
     Path.map ("#r" + i).to (function () {
       if ($('#_r' + i).length) {
         $container.append ($('<div />').addClass ('r').append (_.template ($('#_r' + i).html (), {}) ({})));
-        scrollTo (1, 100)
+        is_load && onYouTubePlayerAPIReady ();
       }
     }).enter (function () {
       $container.children ('.r').addClass ('r-hide').bind ('animationend webkitAnimationEnd MSAnimationEnd oAnimationEnd', function () { $(this).remove (); });
@@ -63,5 +118,4 @@ $(function () {
   });
 
   Path.listen();
-
 });
