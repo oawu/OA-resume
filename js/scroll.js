f6e7c4cdabb05ee6684ebbbed09a57b03670295e
OA Wu <comdan66@gmail.com>
2015-03-30 13:47:36 +0800

    Temp.

--- js/scroll.js
+++ js/scroll.js
@@ -26,16 +26,16 @@ $(function () {
                         if (($(this).offset ().top - $(window).scrollTop ()) < ($(window).height () / 2) && !$(this).hasClass ('now'))
                           now_index = $(this).index ();
                       });
-                      var $ed = $container.children ('.page:lt(' + (now_index) + ')').addClass ('ed')
+                      var $ed = $container.children ('.page:lt(' + (now_index) + ')').addClass ('ed');
                       var $now = $container.children ('.page').eq (now_index).addClass ('now');
                       if (index != now_index && $now.length) {
                         index = now_index;
-                        scrollTo ($now.offset ().top -  range, 100)
+                        scrollTo ($now.offset ().top -  range, 100);
                       }
                     });
                   }, 100);
 
-              $navbar.removeClass ('hide')
+              $navbar.removeClass ('hide');
               $container.removeClass ('hide');
               $footer.removeClass ('hide');
               $loading.fadeOut (function () {
@@ -43,4 +43,37 @@ $(function () {
                 scrollTo (1200, 100);
               });
             });
+
+  var pictureIndex = 0;
+  $('.avatar, .picture').OAimgLiquid ();
+  $('.page').each (function () {
+    pictureIndex += 1;
+    $(this).find ('.picture').each (function () {
+       $(this).attr ('data-fancybox-group', 'group_' + pictureIndex).attr ('href', $(this).find ('img').attr ('src'));
+    });
+  });
+
+  $('.picture').fancybox ({
+    beforeLoad: function() {
+      if ($(this.element).attr ('title'))
+        this.title = $(this.element).attr ('title');
+      else if ($(this.element).attr ('alt'))
+        this.title = $(this.element).attr ('alt');
+      else if ($(this.element).data ('fancybox_title'))
+        this.title = $(this.element).data ('fancybox_title');
+    },
+    padding : 0,
+    helpers : {
+      overlay: {
+        locked: false
+      },
+      title : {
+        type : 'over'
+      },
+      thumbs: {
+        width: 50,
+        height: 50
+      }
+    }
+  });
 });
